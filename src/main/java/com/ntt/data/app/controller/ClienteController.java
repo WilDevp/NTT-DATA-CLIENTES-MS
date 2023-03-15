289de37ed662a9d342877d031d7f96e3aad1bce8
WilDevp <wilmar.garciava@gmail.com>
2023-03-15 18:00:45 -0500

    refactor: :sparkles: Update Controller Package
    Update ClienteController for refactoring
    
    Working: Prueba MS

--- src/main/java/com/ntt/data/app/controller/ClienteController.java
+++ src/main/java/com/ntt/data/app/controller/ClienteController.java
@@ -12,38 +12,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v01/clientes")
 public class ClienteController {
     @GetMapping
-    public ResponseEntity<Cliente> obtenerCliente(@RequestParam String tipoDocumento, @RequestParam String numeroDocumento){
-        if (!tipoDocumento.matches("^[CP]$")){
+    public ResponseEntity<Cliente> obtenerCliente(@RequestParam String tipoDocumento, @RequestParam String numeroDocumento) {
+        if (!tipoDocumento.matches("^[CP]$")) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         if (!numeroDocumento.matches("^\\d+$") && !tipoDocumento.equals("P")) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+        Cliente cliente = obtenerClientePorDocumento(tipoDocumento, numeroDocumento);
 
-        if (tipoDocumento.equals("C") && numeroDocumento.equals("23445322")) {
-            Cliente cliente = new Cliente();
-            cliente.setPrimerNombre("Ares");
-            cliente.setSegundoNombre("Rio");
-            cliente.setPrimerApellido("Garcia");
-            cliente.setSegundoApellido("Guerra");
-            cliente.setTelefono("3001234567");
-            cliente.setDireccion("Calle 123 #45-67");
-            cliente.setCiudad("Medellín");
-
-            return new ResponseEntity<>(cliente, HttpStatus.OK);
-        } else if (tipoDocumento.equals("P") && numeroDocumento.equals("P123456789")) {
-            Cliente cliente = new Cliente();
-            cliente.setPrimerNombre("Maria");
-            cliente.setSegundoNombre("Fernanda");
-            cliente.setPrimerApellido("Rodríguez");
-            cliente.setSegundoApellido("López");
-            cliente.setTelefono("3129876543");
-            cliente.setDireccion("Avenida 456 #78-90");
-            cliente.setCiudad("Medellín");
-
+        if (cliente != null) {
             return new ResponseEntity<>(cliente, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+        private Cliente crearCliente(String primerNombre, String segundoNombre, String primerApellido, String segundoApellido,
+                String telefono, String direccion, String ciudad) {
+            Cliente cliente = new Cliente();
+            cliente.setPrimerNombre(primerNombre);
+            cliente.setSegundoNombre(segundoNombre);
+            cliente.setPrimerApellido(primerApellido);
+            cliente.setSegundoApellido(segundoApellido);
+            cliente.setTelefono(telefono);
+            cliente.setDireccion(direccion);
+            cliente.setCiudad(ciudad);
+            return cliente;
+        }
+    private Cliente obtenerClientePorDocumento(String tipoDocumento, String numeroDocumento) {
+        if (tipoDocumento.equals("C") && numeroDocumento.equals("23445322")) {
+            return crearCliente("Juan", "Carlos", "Pérez", "García", "3001234567", "Calle 123 #45-67", "Bogotá");
+        } else if (tipoDocumento.equals("P") && numeroDocumento.equals("P123456789")) {
+            return crearCliente("Maria", "Fernanda", "Rodríguez", "López", "3129876543", "Avenida 456 #78-90", "Medellín");
+        }
+        return null;
+    }
+
+
 }
